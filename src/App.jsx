01e4c0c36cd5d37a414d381ba7aa065d63f866e4
Pietraceci <pietracecily@gmail.com>
2022-07-01 15:52:18 -0400

    criando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App(){
+    const planos = [
+        {
+            title: "Start",
+            price: "Free",
+            qnt_users: "1 User",
+            qnt_projects: "1 project"
+        },
+        {
+            title: "Basic",
+            price: "$19.99",
+            qnt_users: "5 Users",
+            qnt_projects: "20 Projects"
+        },
+        {
+            title: "Expert",
+            price: "$129.99",
+            qnt_users: "Unlimited Users",
+            qnt_projects: "Unlimited Users"
+        }
+    ];
+	return(
+        <div>
+            
+        </div>
+    );
+
+}
+export default App;
